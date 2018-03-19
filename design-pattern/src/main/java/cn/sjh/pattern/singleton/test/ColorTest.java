26b11920eb2946cdae8f28190b63cd693fe2182f
songjh <525004657@qq.com>
2018-03-19 08:39:52 +0800

    add singleton pattern

---
+++ design-pattern/src/main/java/cn/sjh/pattern/singleton/test/ColorTest.java
@@ -0,0 +1,13 @@
+package cn.sjh.pattern.singleton.test;
+
+
+import cn.sjh.pattern.singleton.register.Color;
+
+/**
+ * Created by Tom on 2018/3/7.
+ */
+public class ColorTest {
+    public static void main(String[] args) {
+        System.out.println(Color.RED);
+    }
+}
